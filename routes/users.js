274041d62d6d5ccbad30f17aa0a63f1fec6a6b09
Thiago Durães <duraes.thiago@gmail.com>
2022-10-07 17:39:49 -0300

    Criada página de usuário e partials alterada

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.render('user-login');
+  res.render('user');
 });
 
 module.exports = router;
