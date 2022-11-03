bda7373571ae7e04f84323283bed0c8c228a5ec2
Thiago Durães <duraes.thiago@gmail.com>
2022-11-03 18:15:46 -0300

    Configuracao Sequelize

---
+++ tests/config.js
@@ -0,0 +1,9 @@
+const config = {
+    username: "root",
+    password: "root",
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    database: "latech"
+}
+module.exports = config;
--- tests/testeConfig 2.js
+++ tests/testeConfig 2.js
@@ -1,16 +1,23 @@
 const sequelize = require('sequelize');
 // 2 - Criar um objeto contendo os dados da conexão
-const config = require('./config.js');
+const config = {
+    username: "root",
+    password: "Balam22",
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    database: "latech"
+}
 // 3 - Cria a conexão
 const conexao = new sequelize(config);
 // 4 - Realiza a consulta (assíncronas)!!!
 let resultado = conexao.query("SELECT * FROM produtos");
 console.log(resultado)
 
-async function levantarProdutos() {
-
+async function levantarProdutos(){
+    
     try {
-        let resultado = await conexao.query("SELECT * FROM produtos");
+        let resultado = await conexao.query("SELECT * FROM produtos LIMIT 0,5");
         console.log(resultado);
     } catch (error) {
         console.log("Deu muito ruim!");
