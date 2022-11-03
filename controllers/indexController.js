4ff20a3377e3722062d309a6e353f58db43c1bda
Thiago Durães <duraes.thiago@gmail.com>
2022-11-03 12:05:42 -0300

    Atualizacao 03-11-2022-2

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,7 +1,7 @@
-const controller =  {
-    index: (req, res)=> {
-        res.render('index', { title: 'Latech' });
-      }
+const controller = {
+  index: (req, res) => {
+    res.render('index', { title: 'PI Latech' });
+  }
 }
 
 module.exports = controller
