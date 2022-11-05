1b6239c6235fe165fbbf388d674ef67fd08b5e00
Thiago Durães <duraes.thiago@gmail.com>
2022-11-05 16:41:38 -0300

    Update config da pasta tests

--- tests/conf.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    username: "root",
-    password: "Balam22",
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    database: "latech"
-}
-module.exports = config;
--- tests/testeConfig.js
+++ tests/testeConfig.js
@@ -1,21 +1,14 @@
 const sequelize = require('sequelize');
 // 2 - Criar um objeto contendo os dados da conexão
-const config = {
-    username: "root",
-    password: "Romana11",
-    host: "localhost",
-    port: 3306,
-    dialect: "mysql",
-    database: "latech"
-}
+const config = require('./config');
 // 3 - Cria a conexão
 const conexao = new sequelize(config);
 // 4 - Realiza a consulta (assíncronas)!!!
 let resultado = conexao.query("SELECT * FROM produtos");
 console.log(resultado)
 
-async function levantarProdutos(){
-    
+async function levantarProdutos() {
+
     try {
         let resultado = await conexao.query("SELECT * FROM produtos LIMIT 0,5");
         console.log(resultado);
