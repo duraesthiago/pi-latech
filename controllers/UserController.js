9db4a351740dcae9c49f521cf55afcf5f82799ea
PaulaFlavia <paulaflavia@gmail.com>
2022-11-29 18:29:49 -0300

    refatorado o processo de cadastro, toca do nome da model Client para User, criado o link para a pag de login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,22 +1,46 @@
-const {validationResult} = require('express-validator');
-
+const { validationResult } = require("express-validator");
 
 const UserController = {
-    userLogin: (req, res) => {res.render('userLogin')},
 
-    showUserAccount: (req, res) => {res.render('userAccount')},
+  userLogin: (req, res) => {
+    res.render("userLogin");
+  },
 
-    signUp: (req, res) => {res.render('userSignUp')},
+  doingLogin: (req, res) => {
 
-    loginValidation: (req, res) => {
-            const resultValidations = validationResult(req); 
-            if(resultValidations.errors.length >0){
-                    return res.render('userLogin', {
-                            errors: resultValidations.mapped(),
-                            oldData: req.body
-                        });
-                    }
-                    }
-}
+  },
+
+
+  showUserAccount: (req, res) => {
+    res.render("userAccount");
+  },
+
+  signUp: (req, res) => {
+    res.render("userSignUp");
+  },
+
+  signUpValidation: (req, res) => {
+    const resultValidations = validationResult(req);
+    if (resultValidations.errors.length > 0) {
+      return res.render("userSignUp", {
+        errors: resultValidations.mapped(),
+        oldData: req.body,
+      });
+    }
+  },
+  logout: (req,res) => {
+    
+  }
+
+  //   let userToCreate = {
+  //     ...req.body,
+  //     password: bcrypt.hashSync(req.body.psw, 10)
+  //   }
+
+  //   let userCreated = User.create(userToCreate);
+  //   return res.redirect('/user/userLogin');
+  // },
+  
+};
 
 module.exports = UserController;
