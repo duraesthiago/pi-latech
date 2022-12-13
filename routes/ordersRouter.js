ded61591841bb32da462fef1f358c88e13f519d8
Sergio <aguilarlondono@gmail.com>
2022-12-13 10:17:56 -0300

    Quantidade atualizada no carrinho rodando OK
    Co-authored-by: Thiago Durães <duraesthiago@users.noreply.github.com>

---
+++ routes/ordersRouter.js
@@ -0,0 +1,19 @@
+const router = require('express').Router()
+const ordersController = require('../controllers/OrdersController')
+
+router.get('/', ordersController.index)
+router.post('/addCart', ordersController.addCart)
+router.get('/cart', ordersController.showCart)
+
+router.put('/updateCart', ordersController.updateCart)
+router.get('/release', ordersController.releaseOrder)
+
+router.put('/updateCart', ordersController.updateCart) 
+
+//router.post('/release', ordersController.releaseOrder)
+//router.put('/cancel/:id', ordersController.cancelOrder)
+
+//router.get('/payment', ordersController.paymentProcess)
+
+module.exports = router
+
