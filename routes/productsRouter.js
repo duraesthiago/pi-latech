70a807e7d212813122a856a3fb1860139178a0d0
Thiago Durães <duraes.thiago@gmail.com>
2022-12-14 09:42:04 -0300

    productsRouter atualizado

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -5,6 +5,17 @@ const router = express.Router();
 // ************ Controller ************
 const productsController = require('../controllers/productsController');
 
+/*** GET ALL PRODUCTS ***/
+router.get('/', productsController.index);
+
+/*** GET OFFER PRODUCTS ***/
+router.get('/sale', productsController.sale);
+
+/*** GET CATEGORY PRODUCTS ***/
+router.get('/category', productsController.category);
+
+/*** GET ONE PRODUCT ***/
+router.get('/detail/:id/', productsController.detail);
 
 /*** EDIT ONE PRODUCT ***/
 router.get('/:id/edit', productsController.edit);
