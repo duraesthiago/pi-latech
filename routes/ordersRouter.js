c786e316671038356d9533cdc206e024b2b826e2
Sergio <aguilarlondono@gmail.com>
2022-12-14 11:10:50 -0300

    resolvendo conflito rota orders

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -5,12 +5,9 @@ router.get('/', ordersController.index)
 router.post('/addCart', ordersController.addCart)
 router.get('/cart', ordersController.showCart)
 
-router.put('/updateCart', ordersController.updateCart)
-
-router.get('/release', ordersController.releaseOrder)
-
 
 //router.post('/remove/:id', ordersController.removeProduct) 
+//router.post('/release', ordersController.releaseOrder)
 //router.put('/cancel/:id', ordersController.cancelOrder)
 
 //router.get('/payment', ordersController.paymentProcess)
