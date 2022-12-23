d64f1e9f2e95865812db47c847e6fa42ed803a8e
PaulaFlavia <paulaflavia@gmail.com>
2022-12-23 14:40:41 -0300

    arrumadas as views de usuarios e novo db

--- database/latech.sql
+++ database/latech.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `latech` /*!40100 DEFAULT CHARACTER SET utf8mb3 */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `latech`;
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
 -- Host: localhost    Database: latech
 -- ------------------------------------------------------
--- Server version	8.0.30
+-- Server version	8.0.31
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -29,10 +29,10 @@ CREATE TABLE `addresses` (
   `Endereco` varchar(100) DEFAULT NULL,
   `Cidade` varchar(45) DEFAULT NULL,
   `Estado` varchar(45) DEFAULT NULL,
-  `users_idClientes` int NOT NULL,
+  `users_idUser` int NOT NULL,
   PRIMARY KEY (`idAddresses`),
-  KEY `fk_addresses_users1_idx` (`users_idClientes`),
-  CONSTRAINT `fk_addresses_users1` FOREIGN KEY (`users_idClientes`) REFERENCES `users` (`idClientes`)
+  KEY `fk_addresses_users1_idx` (`users_idUser`),
+  CONSTRAINT `fk_addresses_users1` FOREIGN KEY (`users_idUser`) REFERENCES `users` (`idUser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -200,15 +200,15 @@ CREATE TABLE `purchases` (
   `idPedidos` int NOT NULL AUTO_INCREMENT,
   `Data_pedido` datetime DEFAULT NULL,
   `Total` decimal(10,2) NOT NULL,
-  `Users_IdClientes` int NOT NULL,
+  `Users_idUser` int NOT NULL,
   `Forma_de_Pagamento` varchar(45) DEFAULT NULL,
   `Endereço_de_Entrega` varchar(100) DEFAULT NULL,
   `addresses_idAddresses` int NOT NULL,
   PRIMARY KEY (`idPedidos`),
-  KEY `fk_Purchases_Users1_idx` (`Users_IdClientes`) /*!80000 INVISIBLE */,
+  KEY `fk_Purchases_Users1_idx` (`Users_idUser`) /*!80000 INVISIBLE */,
   KEY `fk_purchases_addresses1_idx` (`addresses_idAddresses`),
   CONSTRAINT `fk_purchases_addresses1` FOREIGN KEY (`addresses_idAddresses`) REFERENCES `addresses` (`idAddresses`),
-  CONSTRAINT `fk_Purchases_Users1` FOREIGN KEY (`Users_IdClientes`) REFERENCES `users` (`idClientes`)
+  CONSTRAINT `fk_Purchases_Users1` FOREIGN KEY (`Users_idUser`) REFERENCES `users` (`idUser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -230,14 +230,14 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `idClientes` int NOT NULL AUTO_INCREMENT,
+  `idUser` int NOT NULL AUTO_INCREMENT,
   `Nome` varchar(45) NOT NULL,
   `Sobrenome` varchar(45) DEFAULT NULL,
   `Email` varchar(45) NOT NULL,
   `Telefone` varchar(45) DEFAULT NULL,
   `Senha` varchar(100) NOT NULL,
   `Avatar` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`idClientes`)
+  PRIMARY KEY (`idUser`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -260,4 +260,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-12-18 15:11:07
+-- Dump completed on 2022-12-22  9:40:49
