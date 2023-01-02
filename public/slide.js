c24494546b59ce7930ce61326a73670109168103
Arianne Cardoso <comercial.arianne@gmail.com>
2023-01-02 14:35:17 -0300

    slide.js Criação da função activeControlButtons e refatoração do codigo.

--- public/slide.js
+++ public/slide.js
@@ -6,6 +6,7 @@ const navNextBottun = document.querySelector('[data-slide="nav-next-button"]')
 const navPreviousBottun = document.querySelector('[data-slide="nav-previous-button"]')
 const controlsWrapper = document.querySelector('[data-slide="controls-wrapper"]')
 const slideItems = document.querySelectorAll('[data-slide="item"]')
+let controlButtons 
 
 const state = { 
     startingPoint: 0,
@@ -33,6 +34,7 @@ function getCenterPosition({ index }){
 function setVisibleSlide ({ index }){   
     const position = getCenterPosition({ index })
     state.currentSlideIndex = index
+    activeControlButton({ index })
     translateSlide({ position })
 }
 
@@ -53,6 +55,14 @@ function createControlButtons(){
         controlButton.dataset.slide = 'control-button'      
         controlsWrapper.append(controlButton)
     })    
+}
+
+function activeControlButton({ index }){
+    const controlButton = controlButtons[index]
+    controlButtons.forEach(function(controlButtonItem){
+        controlButtonItem.classList.remove('active')
+    })
+    controlButton.classList.add('active')
 }
 
 function onMouseDown(event, index){
@@ -84,21 +94,16 @@ function onMouseUp(event){
     slideItem.removeEventListener('mousemove', onMouseMove)    
 }
 
-function onControlButtonClick(event, index, controlButtons){
-    const controlButton = event.currentTarget
-    controlButtons.forEach(function(controlButtonItem){
-        controlButtonItem.classList.remove('active')
-    })
-    controlButton.classList.add('active')
+function onControlButtonClick(index){    
     setVisibleSlide({ index })
 }
 
 function setListeners(){
-    const controlButtons = document.querySelectorAll("[data-slide='controlButton']")
+    controlButtons = document.querySelectorAll("[data-slide='controlButton']")
 
     controlButtons.forEach(function(controlButton, index){
         controlButton.addEventListener('click', function(event){
-            onControlButtonClick(event, index, controlButtons)
+            onControlButtonClick(index)
         })
     })
 
