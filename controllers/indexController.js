11edf596164b9ad870304244607b40ea81b4f8c5
Arianne Cardoso <comercial.arianne@gmail.com>
2023-01-03 16:14:30 -0300

    commit de rebase

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -9,23 +9,22 @@ const IndexController = {
           model: Image, as: 'images',
       }],
       where: {
-        idProdutos: idProdutosComDesconto
+        Oferta:{[Op.ne]: 0,}
       }
   });    
-    products = products.map((product) => {
-      product.PrecoComDesconto = product.Preco * 0.7
-      return product
-    })
+    // products = products.map((product) => {
+    //   product.PrecoComDesconto = product.Preco * product.Oferta
+    //   return product
+    // })
     console.log(products[0])
     res.render("index", { title: "Latech", products });
   },
 
-
   aboutUs: (req, res) => {
-
 
     res.render("aboutUs");
   },
+  
   search: async (req, res) => {
     let q = req.query.q;
     let products = await Product.findAll({
@@ -39,41 +38,9 @@ const IndexController = {
         }
       }
     })
-    products = products.map(
-      (product) => {
-        if (idProdutosComDesconto.includes(product.id)) {
-          product.PrecoComDesconto = product.Preco * 0.7
-        }else{
-          product.PrecoComDesconto = product.Preco * 1     
-        }        
-        return product
-      })
+    
     res.render("search", { title: "Latech", products });
   },
-  search: async (req, res) => {
-    let q = req.query.q;
-    let products = await Product.findAll({
-      raw: true,
-      include: [{
-          model: Image, as: 'images',
-      }],
-      where: {
-        Nome: {
-          [Op.like]: `%${q}%`
-        }
-      }
-    })
-    products = products.map(
-      (product) => {
-        if (idProdutosComDesconto.includes(product.id)) {
-          product.PrecoComDesconto = product.Preco * 0.7
-        }else{
-          product.PrecoComDesconto = product.Preco * 1     
-        }        
-        return product
-      })
-    res.render("search", { title: "Latech", products });
-  }
 
 };
 
