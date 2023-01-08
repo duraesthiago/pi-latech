95c0251d9e5eb62f9697f841609a3ee8ec926673
PaulaFlavia <paulaflavia@gmail.com>
2023-01-08 11:49:33 -0300

    atualizando views d eusuários e admins Deletado middleware de rota em order

--- routes/ordersRouter.js
+++ routes/ordersRouter.js
@@ -2,12 +2,12 @@ const router = require('express').Router()
 const ordersController = require('../controllers/OrdersController')
 const notLoggedUserMiddleware = require('../middlewares/notLoggedUserMiddleware')
 
-router.get('/', notLoggedUserMiddleware, ordersController.index)
-router.post('/addCart', notLoggedUserMiddleware, ordersController.addCart)
-router.get('/cart', notLoggedUserMiddleware, ordersController.showCart)
+router.get('/', ordersController.index)
+router.post('/addCart', ordersController.addCart)
+router.get('/cart', ordersController.showCart)
 
-router.put('/updateCart', notLoggedUserMiddleware, ordersController.updateCart)
-router.get('/release', notLoggedUserMiddleware, ordersController.releaseOrder)
+router.put('/updateCart', ordersController.updateCart)
+router.get('/release', ordersController.releaseOrder)
 
 router.put('/updateCart', ordersController.updateCart)
 router.get('/release', ordersController.releaseOrder)
@@ -18,7 +18,7 @@ router.get('/payment', ordersController.payment)
 router.get('/payment', ordersController.payment)
 //router.post('/payment', ordersController.payment)
 
-router.post('/release', notLoggedUserMiddleware, ordersController.releaseOrder)
+router.post('/release', ordersController.releaseOrder)
 router.post('/release', ordersController.releaseOrder)
 
 //router.post('/remove/:id', ordersController.removeProduct) 
