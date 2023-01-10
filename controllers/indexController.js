5b9bef7889f1a39147e9fedb2658da63817c8ace
PaulaFlavia <paulaflavia@gmail.com>
2023-01-10 13:47:16 -0300

    identação

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -26,30 +26,30 @@ const IndexController = {
 
     res.render("aboutUs");
   },
-  search: async (req, res) => {
-    let q = req.query.q;
-    let products = await Product.findAll({
-      raw: true,
-      include: [{
-          model: Image, as: 'images',
-      }],
-      where: {
-        Nome: {
-          [Op.like]: `%${q}%`
-        }
-      }
-    })
-    products = products.map(
-      (product) => {
-        if (idProdutosComDesconto.includes(product.id)) {
-          product.PrecoComDesconto = product.Preco * 0.7
-        }else{
-          product.PrecoComDesconto = product.Preco * 1     
-        }        
-        return product
-      })
-    res.render("search", { title: "Latech", products });
-  },
+  // search: async (req, res) => {
+  //   let q = req.query.q;
+  //   let products = await Product.findAll({
+  //     raw: true,
+  //     include: [{
+  //         model: Image, as: 'images',
+  //     }],
+  //     where: {
+  //       Nome: {
+  //         [Op.like]: `%${q}%`
+  //       }
+  //     }
+  //   })
+  //   products = products.map(
+  //     (product) => {
+  //       if (idProdutosComDesconto.includes(product.id)) {
+  //         product.PrecoComDesconto = product.Preco * 0.7
+  //       }else{
+  //         product.PrecoComDesconto = product.Preco * 1     
+  //       }        
+  //       return product
+  //     })
+  //   res.render("search", { title: "Latech", products });
+  // },
   search: async (req, res) => {
     let q = req.query.q;
     let products = await Product.findAll({
