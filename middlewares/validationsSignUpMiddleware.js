054bcef7a7ba28645d88482867b35b402dd024da
Thiago Durães <duraes.thiago@gmail.com>
2023-01-19 11:43:05 -0300

    Alterando CCadminProduct.css

--- middlewares/validationsSignUpMiddleware.js
+++ middlewares/validationsSignUpMiddleware.js
@@ -16,30 +16,30 @@ const validationsSignUp = [
     check('phone_number')
         .notEmpty().withMessage('É necessário digitar seu telefone com ddd').bail()
         .trim(),
-        check('personal_id')
+    check('personal_id')
         .notEmpty().withMessage('É necessário o numero do seu CPF').bail()
-        .isLength({max: 14}).withMessage('Digite apenas os números').bail()
+        .isLength({ max: 14 }).withMessage('Digite apenas os números').bail()
         .trim(),
     check('password')
         .notEmpty().withMessage('É necessário digitar uma senha').bail()
-        .isLength({min: 6}).withMessage('A senha precisa ter no mínimo 6 caracteres').bail()
+        .isLength({ min: 6 }).withMessage('A senha precisa ter no mínimo 6 caracteres').bail()
         .trim(),
-        check('avatar').custom((value, {req}) => { 
-            let file = req.file;
-            let acceptedExtensions = ['.jpg', '.png', '.gif']
-    
-            if(!file){
-                throw new Error('Você precisa escolher um arquivo');
-            } else {
-                let fileExtension =  path.extname(file.originalname);
-                
-                if(!acceptedExtensions.includes(fileExtension)) {
-                    throw new Error(`Os arquivos podem ter as extensões ${acceptedExtensions.join(', ')}`);
-                }
+    check('avatar').custom((value, { req }) => {
+        let file = req.file;
+        let acceptedExtensions = ['.jpg', '.png', '.gif']
+
+        if (!file) {
+            throw new Error('Você precisa escolher um arquivo');
+        } else {
+            let fileExtension = path.extname(file.originalname);
+
+            if (!acceptedExtensions.includes(fileExtension)) {
+                throw new Error(`Os arquivos podem ter as extensões ${acceptedExtensions.join(', ')}`);
             }
-            return true;
+        }
+        return true;
     })
 ];
-   
+
 
 module.exports = validationsSignUp;
